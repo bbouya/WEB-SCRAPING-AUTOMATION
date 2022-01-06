a06b2d14b7211e3aaec8dca4460fb3b99b71e3eb
bbouya <52068209+bbouya@users.noreply.github.com>
2022-01-06 16:50:37 +0100

    Add

--- WebScraping_research/Indeed/Indeedjobscrapper.py
+++ WebScraping_research/Indeed/Indeedjobscrapper.py
@@ -43,12 +43,89 @@ location.send_keys(['Canada'])
 
 
 #click search
-find = driver.find_element_by_xpath("//button[contains(text(),'To research')]")
+find = driver.find_element_by_xpath("//button[contains(text(),'Rechercher')]")
 find.click()
 
 
 
 # AFTER Advanced search:
+advanced_search = driver.find_element_by_xpath("//a[contains(text(),'Recherche avancée')]")
+advanced_search.click()
+
+# Set diplsay limit of 30 results per page.
+display_limit = driver.find_element_by_xpath("//select[@id = 'limit']//option[@value = '30']")
+display_limit.click()
 
 
+search_button = driver.find_element_by_xpath('//*[@id = "fj"]')
+search_button.click()
+
+close_popup = driver.find_element_by_id('popover-x')
+close_popup.click()
+
+
+
+# Let the driver wait 3 seconds to locate the element before exiting out 
+driver.implicitly_wait(3)
+
+titles = []
+companies = []
+locations = []
+links = []
+reviews = []
+salaries = []
+
+# loop
+for i in range(0,20):
+    
+  
+   
+  #job_card = driver.find_elements_by_xpath('//div[contains(@class,"job_seen_beacon")]')
+    job_card = driver.find_elements_by_xpath('//a[contains(@id,"job_")]')
+    
+    for job in job_card:
+       
+    #.  not all companies have review
+        try:
+            review = job.find_element_by_xpath('.//span[@class="ratingNumber"]').text
+        except:
+            review = "None"
+        reviews.append(review)
+        print(review)
+
+        try:
+            location = job.find_element_by_xpath('.//div[contains(@class,"companyLocation")]').text
+        except:
+            location = "None"
+    #.  tells only to look at the element       
+        locations.append(location)
+        print(location)
+
+                                    
+        try:
+            title = job.find_element_by_xpath('.//h1[contains(@class, "icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title is-embedded")]').text
+        except:
+            title = job.find_element_by_xpath('.//h1[contains(@class, "icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title is-embedded")]').get_attribute(name="title")
+        titles.append(title)
+        print(title)
+        
+        
+        links.append(job.get_attribute(name="href"))
+   
+        companies.append(job.find_element_by_xpath('.//span[@class="companyName"]').text)
+        
+        
+    time.sleep( 2 )
+      
+ 
+    try:
+        next_page = driver.find_element_by_xpath('//a[@aria-label={}]//span[@class="pn"]'.format(i+2))
+        next_page.click()
+        
+    except NoSuchElementException:
+        break
+        
+    
+
+    print("Page: {}".format(str(i+2)))           
 print("ayob")
